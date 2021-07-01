adedf2707aa4ed259cbb35acbcc5c713e063d004
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-07-01 10:02:23 -0700

    commit tree 92a7b8f17186884392d466885f085e7d01e80fa6

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+const element = document.createElement('div'); //creates 'div' @ element
+
+document.body.appendChild(element); // makes element 'div' a child of 'body'
+
+const ul = document.createElement('ul'); // creates the 'ul' @ ul
+for (let i = 0; i < 3; i++) {
+debugger;
+  const li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+element.appendChild(ul); //makes 'ul' a child of the 'div'
+
+
 // Write your code here!
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
+document.body.appendChild(newHeader)
 main.remove();
-newHeader.innerHTML = "<h1>Isaac is the champion";
+newHeader.innerHTML = "Isaac is the champion";
+
